25ef70818be4723cf4e7881d51cee96abec9aadc
Антон Светашов <assvetashov@edu.hse.ru>
2017-03-16 21:12:45 +0300

    Added two main classes with 6 characters (each); added new method showCar, which will transfer data to my prog

---
+++ KdzSvetashov/Class1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KdzSvetashov
+{
+    class Car
+    {
+        private string _name;
+        private int _productionYear;
+        private string _engineType;
+        private int _engineСapacity;
+        private string _drivegear;
+        private int _horsepower;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public int ProductionYear
+        {
+            get
+            {
+                return _productionYear;
+            }
+            set
+            {
+                _productionYear = value;
+            }
+        }
+        public string EngineType
+        {
+            get
+            {
+                return _engineType;
+            }
+            set
+            {
+                _engineType = value;
+            }
+        }
+        public int EngineСapacity
+        {
+            get
+            {
+                return _engineСapacity;
+            }
+            set
+            {
+                _engineСapacity = value;
+            }
+        }
+        public string Drivegear
+        {
+            get
+            {
+                return _drivegear;
+            }
+            set
+            {
+                _drivegear = value;
+            }
+        }
+        public int Horsepower
+        {
+            get
+            {
+                return _horsepower;
+            }
+            set
+            {
+                _horsepower = value;
+            }
+
+        }
+
+        public Car(string cName, int year, string enType, int enCapacity, string drGear, int power)
+        {
+            Name = cName;
+            ProductionYear = year;
+            EngineType = enType;
+            EngineСapacity = enCapacity;
+            Drivegear = drGear;
+            Horsepower = power;
+        }
+        public void showCar(KdzSvetashov.MainWindow wnd)
+        {
+            wnd.info.Text += "Машина: " + Name + "\n";
+            wnd.info.Text += "Год выпуска: " + ProductionYear + "\n";
+            wnd.info.Text += "Тип двигателя: " + EngineType + "\n";
+            wnd.info.Text += "Объем двигателя: " + EngineСapacity + "\n";
+            wnd.info.Text += "Привод: " + Drivegear + "\n";
+            wnd.info.Text += "Мощность: " + Horsepower + "\n";
+        }
+
+
+    }
+}
---
+++ KdzSvetashov/Class2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KdzSvetashov
+{
+    class Lorry
+    {
+        private string _name;
+        private int _productionYear;
+        private string _engineType;
+        private int _engineСapacity;
+        private string _freightMass;
+        private int _horsepower;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public int ProductionYear
+        {
+            get
+            {
+                return _productionYear;
+            }
+            set
+            {
+                _productionYear = value;
+            }
+        }
+        public string EngineType
+        {
+            get
+            {
+                return _engineType;
+            }
+            set
+            {
+                _engineType = value;
+            }
+        }
+        public int EngineСapacity
+        {
+            get
+            {
+                return _engineСapacity;
+            }
+            set
+            {
+                _engineСapacity = value;
+            }
+        }
+        public string FreightMass
+        {
+            get
+            {
+                return _freightMass;
+            }
+            set
+            {
+                _freightMass = value;
+            }
+        }
+        public int Horsepower
+        {
+            get
+            {
+                return _horsepower;
+            }
+            set
+            {
+                _horsepower = value;
+            }
+        }
+
+        public Lorry(string lName, int year, string enType, int enCapacity, string frMass, int power)
+        {
+            Name = lName;
+            ProductionYear = year;
+            EngineType = enType;
+            EngineСapacity = enCapacity;
+            FreightMass = frMass;
+            Horsepower = power;
+        }
+
+
+    }
+}
